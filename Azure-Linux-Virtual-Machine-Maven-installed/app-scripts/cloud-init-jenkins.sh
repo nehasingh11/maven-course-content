79da54698345c49a25cdadb3f169e10e6a486661
nehasingh11 <nehasingh.nitm@gmail.com>
2023-09-14 22:10:28 -0400

    sh updated

--- Azure-Linux-Virtual-Machine-Maven-installed/app-scripts/cloud-init-jenkins.sh
+++ Azure-Linux-Virtual-Machine-Maven-installed/app-scripts/cloud-init-jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt install openjdk-11-jre -y
+sudo apt install -y openjdk-17-jdk
+sudo apt install -y openjdk-17-jre
 sudo apt install git -y
 cd /opt
 sudo curl https://dlcdn.apache.org/maven/maven-3/3.9.4/binaries/apache-maven-3.9.4-bin.tar.gz --output apache-maven-3.9.4-bin.tar.gz
